9dd4946899238d26165979ac958e19efa2862e18
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2015-05-19 11:35:01 +0700

    update...

--- public/modules/moviesscheduler/moviesscheduler.js
+++ public/modules/moviesscheduler/moviesscheduler.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
     var rand_colors = ["#66CCCC", "#66CCFF", "#66CC99", "#66FF99", "#FFCCCC", "#FFCCFF", "#00FFFF"];
-    var __current;
+    var __current;//bien dung de luu thong tin khi search movie
     var __icon_drag_movie = document.createElement("img");
     __icon_drag_movie.src = "/img/icon-drag-movie.png";
 
+    var scheduler = {};
+
+    //functions
+    function rtPx(t) {//ham chuyen doi runtime ra pixel
+        var t = t.split(":");
+        return (parseInt(t[0]) * 60 + parseInt(t[1])) * 2;
+    }
     //components
     $("#txt-search").jqxInput({placeHolder: "enter movie name...", source: function (q, r) {
             var d = new $.jqx.dataAdapter(
@@ -33,19 +40,18 @@ $(document).ready(function () {
 
     $("#btn-add").click(function () {
         if (__current) {
-            var m = document.createElement("span");
-            var rnd = Math.floor(Math.random() * rand_colors.length);
-            m.setAttribute("role", "movie-selected");
-            m.setAttribute("data", __current.value.replace(/"/g, "'"));
-            m.setAttribute("draggable", "true");
-            m.style.cssText = "padding:0 12px 0 12px;cursor:pointer;background-color:" + rand_colors[rnd];
-            m.innerHTML = __current.label;
+            var r = Math.floor(Math.random() * rand_colors.length);
+            var o = JSON.parse(__current.value);
+            o.bg = rand_colors[r];
 
-            $("#selected-movies").append(m);
+            var movie = '<div role="movie-selected" draggable="true" style="padding:0 12px 0 6px;display:block;cursor:pointer;background-color:' +
+                    o.bg + ';width:' + rtPx(o.Runtime) + 'px" data="' + JSON.stringify(o).replace(/"/g, "'") +
+                    '"><span role="m-remove" title="remove" style="margin-right:6px;">X</span><span>' + __current.label + '</span></div>';
+            $("#selected-movies").append(movie);
         }
 
         __current = null;
-        $("#txt-search").val("")
+        $("#txt-search").val("");
     });
 
     $("#selected-movies").on("dragstart", "[role=movie-selected]", function (e) {
@@ -55,13 +61,27 @@ $(document).ready(function () {
 
     $("div[role=room]").on("dragover", function (e) {
         e.preventDefault();
+        e.currentTarget.style.border = "5px dashed black";
     });
+
+    $("div[role=room]").on("dragleave", function (e) {
+        e.preventDefault();
+        e.currentTarget.style.border = "";
+    });
+
+    $("#selected-movies").on("click", "[role=m-remove]", function (e) {
+        var n = e.currentTarget.parentNode;
+        var p = e.currentTarget.parentNode.parentNode;
+        p.removeChild(n)
+    });
+
     $("div[role=room]").on("drop", function (e) {
         e.preventDefault();
+        e.currentTarget.style.border = "";
         var self = this, data;
 
         data = JSON.parse(e.originalEvent.dataTransfer.getData("data").replace(/'/g, '"'));
 
-        $(self).append("<div>" + data.IntTitle + "</div>")
+        $(self).append('<div style="background-color:' + data.bg + '">' + data.IntTitle + '</div>');
     });
 });
--- public/modules/moviesscheduler/screen-moviesscheduler.php
+++ public/modules/moviesscheduler/screen-moviesscheduler.php
@@ -5,28 +5,28 @@
             background-color:gray;
             background-image:repeating-linear-gradient(45deg, transparent, transparent 8px, rgba(255,255,255,.5) 8px, rgba(255,255,255,.5) 20px);
         }
-       
+        #selected-movies div{float:left;display:block;}
     </style>
 
     <div style="line-height:30px"><span>MOVIE : </span><span><input id="txt-search" style="border:none;background-color:#eee"></span><span><button id="btn-add" style="margin-left:12px;cursor:pointer;">ADD</button></span></div>
 
     <div style="clear:both;margin-top:12px"></div>
 
-    <div id="selected-movies"><span>SELECTED MOVIES : </span></div>
+    <div id="selected-movies"><div>SELECTED MOVIES : </div></div>
 
-    <div style="clear:both;margin-top:12px"></div>
+    <div style="clear:both;height:12px;"></div>
 
     <div role="room" value="1" style="height:100px;line-height:100px;background-color:#eee;">
         <div style="width:120px;color:#000;margin-right:12px;background-color:#d0e5f5"><span>ROOM<strong> 01 </strong></span><span role="time-start" contenteditable style="float:right">00:00</span></div>
     </div>
 
-    <div style="clear:both;margin-top:12px"></div>
+    <div style="clear:both;height:12px;"></div>
 
     <div role="room" value="2" style="height:100px;line-height:100px;background-color:#eee;">
         <div style="width:220px;color:#000;margin-right:12px;background-color:#d0e5f5"><span>ROOM<strong> 02 </strong></span><span role="time-start" contenteditable style="float:right">00:00</span></div>
     </div>
 
-    <div style="clear:both;margin-top:12px"></div>
+    <div style="clear:both;height:12px;"></div>
 
     <div role="room" value="3" style="height:100px;line-height:100px;background-color:#eee;">
         <div style="width:120px;color:#000;margin-right:12px;background-color:#d0e5f5"><span>ROOM<strong> 03 </strong></span><span role="time-start" contenteditable style="float:right">00:00</span></div>
