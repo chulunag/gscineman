7012ed6112b56f6cb053f974c3e5609633012ff2
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2015-05-20 11:20:12 +0700

    ...

--- public/modules/moviesscheduler/moviesscheduler.js
+++ public/modules/moviesscheduler/moviesscheduler.js
@@ -1,15 +1,25 @@
 $(document).ready(function () {
-    var rand_colors = ["#66CCCC", "#66CCFF", "#66CC99", "#66FF99", "#FFCCCC", "#FFCCFF", "#00FFFF"];
+    var __colors = ["#66CCCC", "#66CCFF", "#66CC99", "#3366FF", "#990099", "#6633CC", "#669999"];
     var __current;//bien dung de luu thong tin khi search movie
     var __icon_drag_movie = document.createElement("img");
     __icon_drag_movie.src = "/img/icon-drag-movie.png";
 
-    var scheduler = {};
+    var __scheduler = [
+        {room: 1, scheduler: {}},
+        {room: 2, scheduler: {}},
+        {room: 3, scheduler: {}}];
 
     //functions
+    function recursiveN() {//ham tra ra node goc' voi N buoc
+
+    }
     function rtPx(t) {//ham chuyen doi runtime ra pixel
         var t = t.split(":");
-        return (parseInt(t[0]) * 60 + parseInt(t[1])) * 2;
+        return (parseInt(t[0]) * 60 + parseInt(t[1])) * 1;
+    }
+
+    function schedulerRender() {
+
     }
     //components
     $("#txt-search").jqxInput({placeHolder: "enter movie name...", source: function (q, r) {
@@ -23,7 +33,7 @@ $(document).ready(function () {
                     r($.map(data.items, function (item) {
                         return {
                             label: item.IntTitle,
-                            value: JSON.stringify({_id: item._id.$id, IntTitle: item.IntTitle, Runtime: item.Runtime})};
+                            value: JSON.stringify({_id: item._id.$id, IntTitle: item.IntTitle, Runtime: item.Runtime, Format: item.Format})};
                     }));
                 }
             });
@@ -35,18 +45,17 @@ $(document).ready(function () {
 
     //events
     $("#txt-search").on("select", function (e) {
-        __current = e.args.item;
+        if (e.args)
+            __current = e.args.item;
     });
 
     $("#btn-add").click(function () {
         if (__current) {
-            var r = Math.floor(Math.random() * rand_colors.length);
             var o = JSON.parse(__current.value);
-            o.bg = rand_colors[r];
+            o.bg = __colors.pop();
 
-            var movie = '<div role="movie-selected" draggable="true" style="padding:0 12px 0 6px;display:block;cursor:pointer;background-color:' +
-                    o.bg + ';width:' + rtPx(o.Runtime) + 'px" data="' + JSON.stringify(o).replace(/"/g, "'") +
-                    '"><span role="m-remove" title="remove" style="margin-right:6px;">X</span><span>' + __current.label + '</span></div>';
+            var movie = '<div role="queue-selected" draggable="true" style="background-color:' + o.bg + ';" data="'
+                    + JSON.stringify(o).replace(/"/g, "'") + '"><img role="m-remove" title="remove" style="margin-right:6px;"><span>' + __current.label + '</span></div>';
             $("#selected-movies").append(movie);
         }
 
@@ -54,9 +63,13 @@ $(document).ready(function () {
         $("#txt-search").val("");
     });
 
-    $("#selected-movies").on("dragstart", "[role=movie-selected]", function (e) {
+    $("#selected-movies").on("dragstart", "[role=queue-selected]", function (e) {
         e.originalEvent.dataTransfer.setData("data", e.currentTarget.attributes.data.value);
         e.originalEvent.dataTransfer.setDragImage(__icon_drag_movie, 24, 24);
+    });
+
+    $("#selected-movies").on("dragstart", "[role=rest]", function (e) {
+        e.originalEvent.dataTransfer.setData("data", e.currentTarget.attributes.data.value);
     });
 
     $("div[role=room]").on("dragover", function (e) {
@@ -70,9 +83,12 @@ $(document).ready(function () {
     });
 
     $("#selected-movies").on("click", "[role=m-remove]", function (e) {
+        //remove phim
         var n = e.currentTarget.parentNode;
         var p = e.currentTarget.parentNode.parentNode;
-        p.removeChild(n)
+        var o = JSON.parse($(n).attr("data").replace(/'/g, '"'));//lay lai thong tin mau sac
+        p.removeChild(n);
+        __colors.push(o.bg);//tra lai mau sac
     });
 
     $("div[role=room]").on("drop", function (e) {
@@ -82,6 +98,20 @@ $(document).ready(function () {
 
         data = JSON.parse(e.originalEvent.dataTransfer.getData("data").replace(/'/g, '"'));
 
-        $(self).append('<div style="background-color:' + data.bg + '">' + data.IntTitle + '</div>');
+        if (data.rest)
+            $(self).append('<div><input role="rest" value="15"></div>');
+        else
+            $(self).append('<div role="on-timeline" style="background-color:' + data.bg + ';width:' + rtPx(data.Runtime) + 'px;">' +
+                    '<div><img role="m-remove" title="remove" style="margin-left:6px;"></div>' +
+                    '<div style="margin-left:6px">' + data.IntTitle + '</div>' +
+                    '<div style="margin-left:6px">' + data.Format + '</div>' +
+                    '<div style="margin-left:6px">start | end</div>' +
+                    '</div>');
+    });
+
+    $("div[role=room]").on("click", "img[role=m-remove]", function (e) {
+        var n = e.currentTarget.parentNode.parentNode;
+        var p = e.currentTarget.parentNode.parentNode.parentNode;
+        p.removeChild(n);
     });
 });
--- public/modules/moviesscheduler/screen-moviesscheduler.php
+++ public/modules/moviesscheduler/screen-moviesscheduler.php
@@ -1,35 +1,39 @@
 <div style="margin-left:12px;">
     <style scoped>
-        div[role=room] div{float:left;}
-        div[role=room] div:first-child{
-            background-color:gray;
-            background-image:repeating-linear-gradient(45deg, transparent, transparent 8px, rgba(255,255,255,.5) 8px, rgba(255,255,255,.5) 20px);
-        }
-        #selected-movies div{float:left;display:block;}
+        div[role=room]{width:1280px;height:42px;background-color:#eee;}
+        div[role=room] > div{float:left;}
+        div[role=room] > div:first-child{width:120px;color:#fff;background-color:#000;}
+        #selected-movies div{float:left;}
+        div[role=queue-selected]{color:#fff;height:21px;line-height:21px;margin-right:6px;margin-left:6px;padding-left:6px;padding-right:6px;cursor:pointer;}
+        input[role=time-start]{width:40px;border:none;text-align:center}
+        div[role=on-timeline]{cursor:pointer;color:#fff;}
+        input[role=rest]{width:40px;border:none;text-align:center;color:#fff;background-color:#000;}
+        img[role=m-remove]{content:url(/img/icon-remove-movie.png);opacity:0.4}
     </style>
 
-    <div style="line-height:30px"><span>MOVIE : </span><span><input id="txt-search" style="border:none;background-color:#eee"></span><span><button id="btn-add" style="margin-left:12px;cursor:pointer;">ADD</button></span></div>
+    <div style="line-height:30px"><span style="margin-right:10px">MOVIE</span><span><input id="txt-search" style="border:none;background-color:#eee"></span><span><button id="btn-add" style="margin-left:12px;cursor:pointer;">ADD</button></span></div>
 
     <div style="clear:both;margin-top:12px"></div>
 
-    <div id="selected-movies"><div>SELECTED MOVIES : </div></div>
+    <div id="selected-movies"><div style="margin-right:10px">SELECTED MOVIES</div><div draggable="true" role="rest" data="{'rest':'1'}" style="background-color:#ccc">REST</div></div>
+
+    <div style="clear:both;height:24px;"></div>
+
+    <div role="room" value="1">
+        <div><span><strong>ROOM 01</strong></span><input role="time-start" value="07:00" maxlength="5" style="margin-left:10px"></div>
+    </div>
+
 
     <div style="clear:both;height:12px;"></div>
 
-    <div role="room" value="1" style="height:100px;line-height:100px;background-color:#eee;">
-        <div style="width:120px;color:#000;margin-right:12px;background-color:#d0e5f5"><span>ROOM<strong> 01 </strong></span><span role="time-start" contenteditable style="float:right">00:00</span></div>
+    <div role="room" value="2">
+        <div><span><strong>ROOM 02</strong></span><input role="time-start" value="07:00" maxlength="5" style="margin-left:10px"></div>
     </div>
 
     <div style="clear:both;height:12px;"></div>
 
-    <div role="room" value="2" style="height:100px;line-height:100px;background-color:#eee;">
-        <div style="width:220px;color:#000;margin-right:12px;background-color:#d0e5f5"><span>ROOM<strong> 02 </strong></span><span role="time-start" contenteditable style="float:right">00:00</span></div>
-    </div>
-
-    <div style="clear:both;height:12px;"></div>
-
-    <div role="room" value="3" style="height:100px;line-height:100px;background-color:#eee;">
-        <div style="width:120px;color:#000;margin-right:12px;background-color:#d0e5f5"><span>ROOM<strong> 03 </strong></span><span role="time-start" contenteditable style="float:right">00:00</span></div>
+    <div role="room" value="3">
+        <div><span><strong>ROOM 03</strong></span><input role="time-start" value="07:00" maxlength="5" style="margin-left:10px"></div>
     </div>
 </div>
 
